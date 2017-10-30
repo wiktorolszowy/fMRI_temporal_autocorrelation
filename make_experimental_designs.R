2617b256093b0fa4fe88cb81a9cba30a6787421b
Wiktor Olszowy <wo222@compute-image.localdomain>
2017-10-30 15:02:21 +0000

    make_experimental_designs.R

---
+++ make_experimental_designs.R
@@ -0,0 +1,43 @@
+
+#-Wiktor Olszowy
+
+path_manage = readLines("path_manage.txt")
+setwd(paste0(path_manage, "/experimental_designs"))
+
+exper_design = function(n, TR, random, off, on, randomOnsets, randomDurations, name) {
+	if (random==F) {
+		onsets = seq(off,n*TR-on,by=off+on)
+		durations = rep(on, length(onsets))
+	} else {
+		onsets = round(randomOnsets[which(randomOnsets+10<n*TR)])
+		durations = round(randomDurations[1:length(onsets)])
+	}
+	stimulus_times = data.frame(onsets=onsets, durations=durations)
+	FSL_output = array(NA, dim=c(3,length(onsets)))
+	FSL_output[1,] = onsets
+	FSL_output[2,] = durations
+	FSL_output[3,] = rep(1,length(onsets))
+	write(FSL_output, file=paste0("FSL_", name, ".txt"), ncolumns=3)
+	for (i in 1:length(onsets)) {
+		if (i==1) {
+			AFNI_output = paste0(onsets[i], ":", durations[i])
+		} else {
+			AFNI_output = paste0(AFNI_output, " ", onsets[i], ":", durations[i])
+		}
+	}
+	write(AFNI_output, file=paste0("AFNI_", name, ".txt"), ncolumns=3)
+}
+
+studies_parameters = read.table(paste0(path_manage, "/studies_parameters.txt"), sep=";", header=T)
+studies = paste0(studies_parameters$study)
+
+for (study_id in 1:length(studies)) {
+   study = studies[study_id]
+   npts = studies_parameters$npts[study_id]
+   TR = studies_parameters$TR[study_id]
+   for (len in seq(10, 40, by=2)) {
+      exper_design(n=npts, TR=TR, random=F, off=len, on=len, name=paste0(study, "_boxcar", len))
+   }
+}
+
+setwd(path_manage)
---
+++ make_parallel_commands.R
@@ -0,0 +1,54 @@
+
+#-Wiktor Olszowy
+
+path_manage        = readLines("path_manage.txt")
+setwd(path_manage)
+studies_parameters = read.table(paste0(path_manage, "/studies_parameters.txt"), sep=";", header=T)
+subject_nos        = studies_parameters$n
+lims               = seq(1, sum(subject_nos)*9+1, by=sum(subject_nos))
+part               = 1
+
+#-AFNI
+id                 = 0
+for (i in 1:length(subject_nos)) {
+   for (j in 1:subject_nos[i]) {
+      id = id + 1
+      #if (id==lims[part+1]) {
+      #   part = part + 1
+      #}
+      cat("export study_id=", i, "; export subject_id=", j, "; bash analysis_for_one_subject_AFNI.sh; \n", file=paste0("parallel_commands/command_", part, "_", id, ".sh"), sep="", append=F)
+   }
+}
+
+#-FSL
+id     = 0
+part   = part+1
+for (i in 1:length(subject_nos)) {
+   for (j in 1:subject_nos[i]) {
+      id = id + 1
+      cat("R -e  'study_id=", i, "; subject_id=", j, "; freq_cutoff_id=1; source(\"analysis_for_one_subject_FSL.R\")' \n", file=paste0("parallel_commands/command_", part,   "_", id, ".sh"), sep="", append=F)
+      cat("R -e  'study_id=", i, "; subject_id=", j, "; freq_cutoff_id=2; source(\"analysis_for_one_subject_FSL.R\")' \n", file=paste0("parallel_commands/command_", part+1, "_", id, ".sh"), sep="", append=F)
+   }
+}
+
+#-SPM
+id   = 0
+part = part+2
+for (i in 1:length(subject_nos)) {
+   for (j in 1:subject_nos[i]) {
+      id = id + 1
+      cat("matlab -r -nodesktop \"study_id=", i, "; subject_id=", j, "; freq_cutoff_id=1; run('analysis_for_one_subject_SPM.m'); exit\" \n", file=paste0("parallel_commands/command_", part,   "_", id, ".sh"), sep="", append=F)
+      cat("matlab -r -nodesktop \"study_id=", i, "; subject_id=", j, "; freq_cutoff_id=2; run('analysis_for_one_subject_SPM.m'); exit\" \n", file=paste0("parallel_commands/command_", part+1, "_", id, ".sh"), sep="", append=F)
+   }
+}
+
+#-statistic maps to MNI space and conduct multiple testing
+id     = 0
+part   = part+2
+for (i in 1:length(subject_nos)) {
+   for (j in 1:subject_nos[i]) {
+      id = id + 1
+      cat("R -e  'study_id=", i, "; subject_id=", j, "; freq_cutoff_id=1; source(\"register_to_MNI_and_do_multiple_testing.R\")' \n", file=paste0("parallel_commands/command_", part,   "_", id, ".sh"), sep="", append=F)
+      cat("R -e  'study_id=", i, "; subject_id=", j, "; freq_cutoff_id=2; source(\"register_to_MNI_and_do_multiple_testing.R\")' \n", file=paste0("parallel_commands/command_", part+1, "_", id, ".sh"), sep="", append=F)
+   }
+}
