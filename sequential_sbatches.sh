3aef7b9803b18a1cb689eb944d40993a5b4cd624
Wiktor Olszowy <olszowyw@gmail.com>
2017-11-29 22:29:34 +0000

    Update sequential_sbatches.sh

--- sequential_sbatches.sh
+++ sequential_sbatches.sh
@@ -8,16 +8,16 @@ set -o errexit
 mkdir out_err
 
 #-AFNI
-part=1; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
+part=1; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
 
 #-FSL
-part=2; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
-part=3; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
+part=2; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
+part=3; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
 
 #-SPM
-part=4; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
-part=5; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
+part=4; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
+part=5; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
 
 #-transform results to MNI space and do cluster inference, all through FSL
-part=6; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
-part=7; export part; sbatch --array=1-858 slurm_submit.array.hphi; sleep 60
+part=6; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
+part=7; export part; sbatch --array=1-780 slurm_submit.array.hphi; sleep 60
